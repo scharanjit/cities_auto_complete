beafeddffd527360daf98b9f444bfa1e01d9c07c
Charanjit Singh <v-charanjit.s@turvo.com>
2018-06-12 10:15:51 +0530

    Code Base

---
+++ src/main/java/org/imaginea/demo/model/Cities.java
@@ -0,0 +1,18 @@
+package org.imaginea.demo.model;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author Charanjit Singh
+ *
+ * cities
+ *
+ */
+
+public class Cities {
+
+    public static List<String> cities = Arrays.asList("Chakragaon S.O");
+}
